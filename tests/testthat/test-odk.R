942942a7f4941523e70a8cb74ced8f0a31289cd7
Nishant Kishore <nish.kishore@gmail.com>
2025-11-18 11:43:36 -0500

    ❇️ `list_odk_projects` and `list_odk_forms`

--- tests/testthat/test-odk.R
+++ tests/testthat/test-odk.R
@@ -1,3 +1,8 @@
 test_that("ODK Connection Test Caches Token", {
   expect_true(init_odk_connection(testing = T, verbose = F))
 })
+
+test_that("ODK API is live", {
+  expect_true(list_odk_projects(url = "https://rblf.tccodk.org/", testing = T) == 404.1)
+  expect_true(list_odk_forms(url = "https://rblf.tccodk.org/", testing = T) == 404.1)
+})
